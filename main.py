4f3222e5b480bda5ee4514587dbcbc8d8d5c7fc4
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-07-06 17:21:18 +0300

    Test commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from collision_map import create_collision_map
 
 if __name__ == "__main__":
     robot = Robot()
-    volkan_boy = 1
+    volkan_boy = 31
     collision_map = create_collision_map(robot)
