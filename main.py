1ce80d3201b5deebad83950d7160320f173ae010
bataseven <bataseven15@ku.edu.tr>
2021-07-06 17:16:11 +0300

    Test commit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from robot import Robot
 from collision_map import create_collision_map
 
-
-
 if __name__ == "__main__":
-
-	robot = Robot()
-	collision_map = create_collision_map(robot)
-	pass
+    robot = Robot()
+    collision_map = create_collision_map(robot)
