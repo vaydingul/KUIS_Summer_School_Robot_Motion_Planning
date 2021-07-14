80b8507aed5f6461dad5333e44a6ae58b39b50ee
bataseven <bataseven15@ku.edu.tr>
2021-07-14 13:28:00 +0300

    minik br mesic

--- potential.py
+++ potential.py
@@ -6,5 +6,5 @@ from config import *
 
 def calculate_potential_map(collision_map, alpha_values, beta_values):
     x_grid, y_grid = np.meshgrid(alpha_values, beta_values)
-    map_grid = np.vstack((x_grid.reshape(np.prod(x_grid.shape), ), y_grid.reshape(np.prod(y_grid.shape), ))).T
+    map_grid = np.vstack((x_grid.reshape((np.prod(x_grid.shape), ), order='F'), y_grid.reshape((np.prod(y_grid.shape), ), order='F'))).T
     pass
