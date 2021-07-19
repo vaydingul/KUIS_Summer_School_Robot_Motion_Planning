a2f6820de807f858d73f12f6f7f9c9a76a0072c3
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-07-19 23:49:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from config import *
 
 if __name__ == "__main__":
-		print(CONFIG['link_1_length'])
+
+
 		collision_map, alpha_axis, beta_axis = create_collision_map()
 		potential_map = calculate_potential_map(collision_map, alpha_axis, beta_axis)
 		plt.scatter(collision_map[:, 0], collision_map[:, 1], cmap='hot')
